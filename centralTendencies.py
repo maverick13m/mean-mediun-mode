e087f9951fc9fda33fa8621eebf37d3334a275e3
maverick13m <71173739+maverick13m@users.noreply.github.com>
2021-06-18 17:06:59 +0530

    Add files via upload

---
+++ centralTendencies.py
@@ -0,0 +1,35 @@
+#Central tendency is a value that tries to describe a data by identifying the central position within the given data.
+# Mean
+# Median
+# Mode
+
+a = [23,4,5,6,8,2,8,20,9,47,5,5,8,5,8,5,8,7,7,7,7,7,7,2,2,2,5,5,5,5]
+
+# Mean
+s = sum(a)
+print(s)
+mean = s / len(a)
+print(mean)
+
+# median
+a.sort()# arrange in assending order
+print(a,len(a))
+median=(a[14]+a[15])/2 #avg. of middle value
+print(median)
+
+
+# mode- 5 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
